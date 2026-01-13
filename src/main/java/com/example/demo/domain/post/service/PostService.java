96dd38bd87e98fdc1e3fe738f31b95e70c24a95b
lotocol <cabbz987@naver.com>
2026-01-13 19:57:00 +0900

    예외 처리 추가

--- src/main/java/com/example/demo/domain/post/service/PostService.java
+++ src/main/java/com/example/demo/domain/post/service/PostService.java
@@ -1,11 +1,11 @@
 package com.example.demo.domain.post.service;
 
 import com.example.demo.domain.post.entity.Post;
+import com.example.demo.domain.post.exception.PostNotFoundException;
 import com.example.demo.domain.post.repository.PostRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
-import java.beans.Transient;
 import java.util.List;
 import java.util.UUID;
 
@@ -18,6 +18,7 @@ public class PostService {
         this.postRepository = postRepository;
     }
 
+    @Transactional
     public void create(String title, String content) {
         Post post = new Post(title, content);
         postRepository.save(post);
@@ -30,18 +31,21 @@ public class PostService {
     @Transactional
     public void update(UUID id, String title, String content) {
         Post post = postRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("게시글 없음"));
+                .orElseThrow(PostNotFoundException::new);
 
         post.update(title, content);
     }
 
+    @Transactional
     public void delete(UUID id) {
-        postRepository.deleteById(id);
+        Post post = postRepository.findById(id)
+                .orElseThrow(PostNotFoundException::new);
+        postRepository.delete(post);
     }
 
     // 게시글 단건 조회
     public Post findById(UUID id) {
         return postRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("게시글 없음"));
+                .orElseThrow(PostNotFoundException::new);
     }
 }
